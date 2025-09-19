f4418f29c13c1afc656be1458e552f3ee3e02fa1
Rian-rgb <rian3903@gmail.com>
2025-09-19 17:28:40 +0700

    major update v3

---
+++ v3/go.mod
@@ -0,0 +1,3 @@
+module github.com/Rian-rgb/module-management/v3
+
+go 1.24.4
---
+++ v3/pointer/pointer.go
@@ -0,0 +1,9 @@
+package pointer
+
+func Of[T any](x T) *T {
+	return &x
+}
+
+func OfString(x string) *string {
+	return &x
+}
